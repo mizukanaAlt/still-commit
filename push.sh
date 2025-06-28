880a4f51b31d047a2955b952939ed921970224f5
mizukanaAlt <rina301087@gmail.com>
2025-06-28 19:11:14 +0000

    FIX3

--- push.sh
+++ push.sh
@@ -1,5 +1,10 @@
 git config --local user.email "rina301087@gmail.com"
 git config --local user.name "mizukanaAlt"
+git config --global user.name "mizukanaAlt"
+git config --global user.password "kana301006"
+git config --global user.email "rina301087@gmail.com"
+git config --global credential.helper store
+echo "https://mizukanaAlt:$GH_TOKEN@github.com" > ~/.git-credentials
 git add -A
 
 arr[0]="bot: 👋 Hello Github!"
