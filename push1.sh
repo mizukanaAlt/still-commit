20bfdc12b1794fc99040c95b7137b1f6743f546e
mizukanaAlt <rina301087@gmail.com>
2025-06-28 19:27:47 +0000

    CRYING1

---
+++ push1.sh
@@ -0,0 +1,7 @@
+git config --local user.email "rina301087@gmail.com"
+git config --local user.name "mizukanaAlt"
+git config --global user.name "mizukanaAlt"
+git config --global user.password "kana301006"
+git config --global user.email "rina301087@gmail.com"
+git config --global credential.helper store
+echo "https://mizukanaAlt:"$GH_TOKEN"@github.com" > ~/.git-credentials
