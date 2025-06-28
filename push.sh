52519fcb65a8a1c1dd6ed143f5fba947f115a017
mizukanaAlt <rina301087@gmail.com>
2025-06-28 19:25:11 +0000

    TRY

--- push.sh
+++ push.sh
@@ -1,24 +1,3 @@
-git config --local user.email "rina301087@gmail.com"
-git config --local user.name "mizukanaAlt"
-git config --global user.name "mizukanaAlt"
-git config --global user.password "kana301006"
-git config --global user.email "rina301087@gmail.com"
-git config --global credential.helper store
-echo "https://mizukanaAlt:"$GH_TOKEN"@github.com" > ~/.git-credentials
-git add -A
-
-arr[0]="bot: 👋 Hello Github!"
-arr[1]="bot: 🥳 Yeayyy!"
-arr[2]="bot: 😬 Working from github."
-arr[3]="bot: 👨‍💻 Work, work, work!"
-arr[4]="bot: 😪 Hufft..."
-arr[5]="bot: 😎 I'm working for my master!"
-arr[6]="bot: 🙄 Running task, again."
-arr[7]="bot: 👻 Thanks master."
-
-d=`date '+%Y-%m-%dT%H:%M:%SZ'`
-rand=$[$RANDOM % ${#arr[@]}]
-          
 git commit -m "${arr[$rand]} (at ${d})
 
 Co-authored-by: mizuenaAlt <enashinonome264@gmail.com>"
